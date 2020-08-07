f6c22b163e7cdb59bc126d56c994b5681fb6a1ab
root <root@apt-cache.lab.int.overly.dev>
2020-08-07 19:57:32 +0000

    Functioning installer and hosting scripts

--- cacher-install.sh
+++ cacher-install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# get IP address
+thisIP=$( hostname -I | sed 's/ //g')
+
 # apt stuff
 apt update
 apt upgrade
 apt install apt-cacher-ng -y
 
 # make sure service is enabled
-if ! systemctl is-enabled apt-cacher-ng ; then
-  systemctl enable apt-cacher-ng
+systemctl enable apt-cacher-ng
 
 # firewall stuff
 ufw allow 3142/tcp
 ufw reload
+
+# tunnel HTTPS
+echo "PassThroughPattern: ^(.*):443$" >> /etc/apt-cacher-ng/acng.conf
+
+# enable cache for this device
+echo 'Acquire::http { Proxy "http://'"$thisIP"':3142"; }' | tee -a /etc/apt/apt.conf.d/proxy
---
+++ start-hosting.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd apt-cacher-source-edit
+
+python3 -m http.server 8000
